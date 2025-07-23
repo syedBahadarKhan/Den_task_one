393de289ebd40083b9cfb2f62af0e67044f2380d
syedBahadarKhan <ba4646465@gmail.com>
2025-07-24 00:10:24 +0500

    the footer section has completed and hambuger in the navbar is added

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-import { animate, stagger } from "motion"
- import { animate, stagger } from "https://cdn.jsdelivr.net/npm/motion@12.23.0/+esm"
+// import { animate, stagger } from "motion"
+//  import { animate, stagger } from "https://cdn.jsdelivr.net/npm/motion@12.23.0/+esm"
 
- animate(".example li", { opacity: 1, y: [50, 0] }, { delay: stagger(0.05) })
-animate(
-  "li",
-  { y: 0, opacity: 1 },
-  { delay: stagger(0.1) }
-)
+//  animate(".example li", { opacity: 1, y: [50, 0] }, { delay: stagger(0.05) })
+// animate(
+//   "li",
+//   { y: 0, opacity: 1 },
+//   { delay: stagger(0.1) }
+// )
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("nav-menu");
+
+
+    hamburger.addEventListener("click", function () {
+     
+      navMenu.classList.toggle("show");
+    });
+
+    // Close menu on link click
+    document.querySelectorAll(".nav-items a").forEach(link => {
+      link.addEventListener("click", () => {
+       
+        navMenu.classList.remove("show");
+      });
+    });
+  });
+
